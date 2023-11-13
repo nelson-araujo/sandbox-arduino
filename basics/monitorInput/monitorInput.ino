50a569fdb000b1e42aadf700c0caabf463c0b913
Nelson Araujo <nelson@nelsonaraujo.com>
2023-11-13 07:53:39 -0500

    add: basic monitor input

---
+++ basics/monitorInput/monitorInput.ino
@@ -0,0 +1,15 @@
+// Monitor input
+
+int PIN = 1;
+
+void setup(){
+    Serial.begin(115200);
+
+    pinMode(PIN, INPUT);
+}
+
+void loop(){
+    int pinReadOut = analogRead(PIN);
+
+    Serial.println(pinReadOut);
+}
---
+++ basics/pulseLed/pulseLed.ino
@@ -0,0 +1,40 @@
+// LED pulsades using an analog pin
+
+// Globals
+int GREEN_LED = 11;
+int LED_LUMEN_DELTA = 5;
+int LED_DELAY = 25;
+
+int ledLumen = 0;
+bool increaseLumen = true;
+
+
+// Setup
+void setup(){
+    Serial.begin(115200);
+
+    pinMode(GREEN_LED, OUTPUT);
+}
+
+// Loop
+void loop(){
+    Serial.println((String)increaseLumen + " - " + (String)ledLumen);
+    // Serial.println(ledLumen);
+    delay(LED_DELAY);
+
+    analogWrite(GREEN_LED, ledLumen);
+
+    if(increaseLumen){
+        if(ledLumen >= 255){
+            increaseLumen = false;
+        } else {
+            ledLumen = ledLumen + LED_LUMEN_DELTA;
+        }
+    } else {
+        if(ledLumen <= 10){
+            increaseLumen = true;
+        } else{
+            ledLumen = ledLumen - LED_LUMEN_DELTA;
+        }
+    }
+}
